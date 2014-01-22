f478851e48a9a35e37f52ec324dc896fc49e0c76
Charlie Beckwith <charlie.beckwith@marquette.edu>
2014-01-21 23:28:28 -0600

    Deleted some stuff

--- DSA2/src/StringAlgs.java
+++ DSA2/src/StringAlgs.java
@@ -98,9 +98,7 @@ public class StringAlgs {
 		 * PSEUDOCODE:
 		 *  
 		 */
-		 // Comment Longer!
-		
-		//SUck iT
+		 
 		String longest = "";
 		String current = "";
 		boolean isPalindrome = false;
